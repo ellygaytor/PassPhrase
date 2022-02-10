74c90616d5df2508dcb0d8737010b200bc86ad52
Ellygaytor <github@dmeskin.xyz>
2022-02-10 12:04:42 -0500

    Add function to capitalize the first letter of a string

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ function getRandomIntInclusive(min, max) {
 	return Math.floor(randomNumber * (max - min + 1)) + min;
 }
 
-
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 let thePassword = null;
 String.prototype.plural = function (revert) {
